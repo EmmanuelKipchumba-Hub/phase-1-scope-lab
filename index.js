7787eaced805da7d844ad33bafd4fc1c227a866a
Emmanuel <emmanuel.k.kipchumba@gmail.com>
2025-08-20 05:23:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+     return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    var bestCustomer = "not bob";
+   
+    console.log("Poor Bob:",); 
+    console.log("poor us:",);
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = "maybe bob";
+    return overwriteBestCustomer;
+}
+
+function leastFavoriteCustomer() {
+    const leastFavoriteCustome = "maybe bob";
+    return leastFavoriteCustome;
+}
+  
+function changeLeastFavoriteCustomer() {
+ var leastFavoriteCustome = "Malley";
+ return leastFavoriteCustome;
+}
